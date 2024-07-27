551ea55ec61482fcef9aea253ae2c4fe2a7f9b94
raunakram <raunakron4@gmail.com>
2024-07-27 21:35:00 +0530

    first commit

---
+++ chats/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class UserProfileModel(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    name = models.CharField(blank=True, null=True, max_length=100)
+    online_status = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.user.username
+
+
+class ChatModel(models.Model):
+    sender = models.CharField(max_length=100, default=None)
+    message = models.TextField(null=True, blank=True)
+    thread_name = models.CharField(null=True, blank=True, max_length=50)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.message
+    
+class ChatNotification(models.Model):
+    chat = models.ForeignKey(to=ChatModel, on_delete=models.CASCADE)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    is_seen = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.user.username
+
